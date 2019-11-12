e70f8ea45998e34c1e24975642104f5f7b028069
Jessica Tran <jessica.tran13@hotmail.com>
2019-11-12 02:29:42 -0800

    More Test cases.

--- PolyCodeTests1/CalculusCalculatorTests.cs
+++ PolyCodeTests1/CalculusCalculatorTests.cs
@@ -16,13 +16,47 @@ namespace MP2.Tests
         [TestMethod()]
         public void IsValidTest1()
         {
-            string polynomialTest1 = "3.0 4 -8.0 0";
-            bool isValid = tests.IsValidPolynomial(polynomialTest1);
+            string test1 = "3.0 4 -8.0 0";
+            bool isValid = tests.IsValidPolynomial(test1);
 
             Assert.IsTrue(isValid);
         }
 
+        [TestMethod()]
+        public void IsValidTest2()
+        {
+            string test2 = "2x^2+1";
+            bool isValid = tests.IsValidPolynomial(test2);
 
+            Assert.IsFalse(isValid);
+        }
+
+        [TestMethod()]
+        public void IsValidTest3()
+        {
+            string test3 = "0";
+            bool isValid = tests.IsValidPolynomial(test3);
+
+            Assert.IsTrue(isValid);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(System.InvalidOperationException))]
+        public void GetPolynomialStringTest1()
+        {
+            tests.GetPolynomialString();
+            
+        }
+
+        [TestMethod]
+        public void GetRootsTest1()
+        {
+            List<double> calculatedRoots = new List<double>;
+            List<double> expectedRoots = new List<double>;
+
+            //calculatedRoots = CalculusCalculator.GetAllRoots(0.0001);
+
+        }
 
     }
 }
